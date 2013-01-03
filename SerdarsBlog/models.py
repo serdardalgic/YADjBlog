475bcb51b025949e2df12de214d38b9afe9e3bb7
Serdar Dalgıç <sd@serdardalgic.org>
2013-01-03 13:26:52 +0200

    Remove commented out lines, author field to the top.

--- SerdarsBlog/models.py
+++ SerdarsBlog/models.py
@@ -7,32 +7,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
+    author = models.ForeignKey(User)
     title = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User)
-    # created_on = models.DateTimeField(editable=False)
-    # modified = models.DateTimeField()
 
     def __unicode__(self):
         return self.title
-
-    #def get_absolute_url(self):
-        #return ('blog_post_detail', (),
-                #{
-                    #'slug' :self.slug,
-                #})
-
-    #def save(self, *args, **kwargs):
-        #if not self.slug:
-            #self.slug = slugify(self.title)
-
-        ##''' On save, update timestamps '''
-
-        ##if not self.id:
-            ##self.created_on = datetime.datetime.today()
-        ##self.modified = datetime.datetime.today()
-        #super(Post, self).save(*args, **kwargs)
 
 class Comment(models.Model):
     name = models.CharField(max_length=50)
