7b14aac090f468aa8e201cc5116bfd502e9cd5db
Serdar Dalgıç <sd@serdardalgic.org>
2013-01-03 13:49:14 +0200

    Minor modifications in models.py

--- SerdarsBlog/models.py
+++ SerdarsBlog/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User)
-    title = models.CharField(max_length=60)
+    title = models.CharField(max_length=100)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
 
@@ -16,6 +16,7 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
+    # TODO: Comments can be linked with parent relationship
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=80)
     website = models.URLField(max_length=200, null=True, blank=True)
