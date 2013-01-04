8ad8c324df75dd1e97a3cd91c73004ce1fd57a53
Serdar Dalgıç <sd@serdardalgic.org>
2013-01-04 06:19:21 +0200

    Some more refactor in SerdarsBlog.urls

--- SerdarsBlog/urls.py
+++ SerdarsBlog/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 
-urlpatterns = patterns('',
+urlpatterns = patterns('SerdarsBlog.views',
     # Examples:
     # url(r'^$', 'DjangoBlogProject.views.home', name='home'),
     # url(r'^DjangoBlogProject/', include('DjangoBlogProject.foo.urls')),
 
-    url(r'^$', 'SerdarsBlog.views.home', name='home'),
-    url(r'^(\d+)/$', 'SerdarsBlog.views.post', name='post'),
-    url(r'^logout/$', 'SerdarsBlog.views.logout_page'),
-    url(r'^login/$', 'SerdarsBlog.views.login_page'),
+    url(r'^$', 'home', name='home'),
+    url(r'^(\d+)/$', 'post', name='post'),
+    url(r'^logout/$', 'logout_page'),
+    url(r'^login/$', 'login_page'),
 )
