e05eef4e6f5b9ac712f9411e7583ae7203374246
Serdar Dalgıç <sd@serdardalgic.org>
2013-01-09 14:00:04 +0200

    Add UserProfile to django admin.

--- SerdarsBlog/admin.py
+++ SerdarsBlog/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
-from SerdarsBlog.models import Post
+from SerdarsBlog.models import Post, UserProfile
 
 class PostAdmin(admin.ModelAdmin):
     search_fields = ['title']
 
+class UserProfileAdmin(admin.ModelAdmin):
+    search_fields = ['user']
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
 
