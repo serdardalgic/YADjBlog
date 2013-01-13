6b3ea37d5260d416398ac0a38db85ba48d482027
Serdar Dalgıç <sd@serdardalgic.org>
2013-01-13 20:49:01 +0200

    Add is_active field to the UserAdmin page in django-admin.

--- SerdarsBlog/admin.py
+++ SerdarsBlog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from SerdarsBlog.models import Post, UserProfile
 
@@ -10,6 +12,11 @@ class PostAdmin(admin.ModelAdmin):
 class UserProfileAdmin(admin.ModelAdmin):
     search_fields = ['user']
     list_display = ['user', 'is_verified']
+
+UserAdmin.list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
