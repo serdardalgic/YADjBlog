3d6bc0967414d15c09e976348d6f9bc1062a6e84
Serdar Dalgıç <sd@serdardalgic.org>
2013-01-17 09:50:57 +0200

    Add debug-toolbar to the project.

--- settings.py
+++ settings.py
@@ -106,6 +106,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'YADjBlog.urls'
@@ -133,6 +134,7 @@ CORE_APPS = (
 
 EXTERNAL_APPS = (
         'south',
+        'debug_toolbar',
         )
 
 INTERNAL_APPS = (
